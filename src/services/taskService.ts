890cf2802128b415c6165a6dd41df9d407db71a3
wagner <wagnerparnoffpereira@gmail.com>
2026-04-14 01:56:12 -0300

    feat: add list and remove tasks

---
+++ src/services/taskService.ts
@@ -0,0 +1,40 @@
+import { read } from 'node:fs';
+import { Task } from '../types/Task';
+import { readTasks, saveTasks } from '../utils/file';
+
+export function addTask(title: string): Task {
+    const tasks = readTasks();
+    
+    const newTask: Task = {
+        id: Date.now(),
+        title,
+        completed: false,
+        createdAt: new Date(),
+    }
+
+    tasks.push(newTask);
+    saveTasks(tasks);
+
+    return newTask;
+}
+
+export function listTasks(): Task[] {
+    return readTasks();
+}
+
+export function completedTasks(id: number): void {
+    const tasks = readTasks();
+
+    const task = tasks.find((t: Task) => t.id === id);
+    if (!task) throw new Error("Task not found")
+    
+    task.completed = true;
+    saveTasks(tasks);
+}
+
+export function removeTask(id: number): void {
+    const tasks = readTasks();
+
+    const fileteredTasks = tasks.filter((t: Task) => t.id !== id);
+    saveTasks(fileteredTasks);
+}
